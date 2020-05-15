056738b831bdc2709974ad9a6bfa7979e3452bbb
cesar214567 <cesar.madera@utec.edu.pe>
2020-05-15 02:07:49 -0500

    minor fixed and insert_register

--- DHash/show_register.cpp
+++ DHash/show_register.cpp
@@ -7,11 +7,8 @@ using namespace std;
 
 int main(int argc, char** argv) {
     DHash<char*> fr("file.dat");
-    char data[5];
-    for(int i=0;i<5;i++){
-        data[i]=argv[1][i];
-    }
-    auto f=fr.search(data);
+
+    auto f=fr.search(argv[1]);
     f.showData();
 
 
